46de1a16906948b432561ddbbb6fa78a691cef7f
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-07-01 11:39:40 -0300

    Criação da classe utils para calculo de metro quadrado

---
+++ src/main/java/br/com/aula2/tm/metroquadrado/utils/MetroQuadrado.java
@@ -0,0 +1,42 @@
+package br.com.aula2.tm.metroquadrado.utils;
+
+import br.com.aula2.tm.metroquadrado.model.Comodo;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MetroQuadrado {
+    public static double metrosQuadrado(double largura, double comprimento) {
+        return largura * comprimento;
+    }
+
+    public static double valorMetroQuadrado(double totalMetrosQuadrados) {
+        return totalMetrosQuadrados * 800;
+    }
+
+    public static Comodo maiorComodo(List<Comodo> comodos) {
+        Comodo maior = comodos.get(0);
+
+        for (Comodo comodo : comodos) {
+            double metroQuadrado = metrosQuadrado(comodo.getComprimento(), comodo.getLargura());
+            double metroQuadradoMaior = metrosQuadrado(maior.getLargura(), maior.getComprimento());
+
+            if(metroQuadrado > metroQuadradoMaior) {
+                maior = comodo;
+            }
+        }
+
+        return maior;
+    }
+
+    public static Map<Comodo, Double> metroQuadradoComodo(List<Comodo> comodos) {
+        Map<Comodo, Double> res = new HashMap<>();
+
+        for(Comodo comodo : comodos) {
+            res.put(comodo, metrosQuadrado(comodo.getLargura(), comodo.getComprimento()));
+        }
+
+        return res;
+    }
+}
