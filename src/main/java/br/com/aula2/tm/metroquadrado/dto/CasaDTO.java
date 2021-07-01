d3bb9ede8a92ee6778a613205ec1586d87315104
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-07-01 11:46:56 -0300

    Criação do DTO para a Casa

---
+++ src/main/java/br/com/aula2/tm/metroquadrado/dto/CasaDTO.java
@@ -0,0 +1,55 @@
+package br.com.aula2.tm.metroquadrado.dto;
+
+import br.com.aula2.tm.metroquadrado.model.Comodo;
+
+import java.util.List;
+import java.util.Map;
+
+public class CasaDTO {
+
+    private String nome;
+    private String endereco;
+    private List<Comodo> comodos;
+    private Double metrosQuadrados;
+    private Double valor;
+    private Comodo maiorComodo;
+    private Map<Comodo, Double> comodoMetroQuadrado;
+
+    public CasaDTO(String nome, String endereco, List<Comodo> comodos, Double metrosQuadrados, Double valor, Comodo maiorComodo, Map<Comodo, Double> comodoMetroQuadrado) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.comodos = comodos;
+        this.metrosQuadrados = metrosQuadrados;
+        this.valor = valor;
+        this.maiorComodo = maiorComodo;
+        this.comodoMetroQuadrado = comodoMetroQuadrado;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public List<Comodo> getComodos() {
+        return comodos;
+    }
+
+    public Double getMetrosQuadrados() {
+        return metrosQuadrados;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public Comodo getMaiorComodo() {
+        return maiorComodo;
+    }
+
+    public Map<Comodo, Double> getComodoMetroQuadrado() {
+        return comodoMetroQuadrado;
+    }
+}
